9ed0960015b2c7be85acb202438346788ec271d5
Evan Smith <evansmith@Evans-Air.lan>
2023-08-17 12:58:33 -0700

    Added table sorting

--- force-app/main/default/lwc/storeOpportunityTable/storeOpportunityTable.js
+++ force-app/main/default/lwc/storeOpportunityTable/storeOpportunityTable.js
@@ -27,6 +27,10 @@ export default class StoreOpportunityTable extends LightningElement {
     @track opportunityData = [];
     columns = COLUMNS;
     @track totalAmount = 0;
+    @track sortBy='Amount'; 
+    @track sortDirection='asc'; 
+
+    renderedCallbackCalled = false;
 
     @wire(getObjectInfo, { objectApiName: STORE_OBJECT})
     storeInfo;
@@ -34,21 +38,26 @@ export default class StoreOpportunityTable extends LightningElement {
     @wire(getPicklistValues, { recordTypeId: '$storeInfo.data.defaultRecordTypeId', fieldApiName: CITY_FIELD})
     cityPicklistValues;
 
+    //Fetches data after rendering complete and sums up total amount
     renderedCallback(){
-        this.fetchOpportunities();
-        this.calculateTotalAmount();
+        if(this.renderedCallbackCalled === false){
+            console.log('renderedCallback');
+            this.fetchOpportunities();
+            this.renderedCallbackCalled = true;
+        }
     }
 
     handleStoreChange(event) {
         this.selectedStore = event.detail.value;
         this.fetchOpportunities();
-        this.calculateTotalAmount();
+        
     }
 
     fetchOpportunities() {
         getOpportunitiesByCity({ city: this.selectedStore })
             .then((result) => {
                 this.opportunityData = result;
+                this.calculateTotalAmount();
             })
             .catch((error) => {
                 console.error('Error fetching opportunities:', error);
@@ -58,9 +67,33 @@ export default class StoreOpportunityTable extends LightningElement {
     calculateTotalAmount() {
         let total = 0;
         this.opportunityData.forEach((opp) => {
-            total += opp.amount; // Replace 'amount' with the actual API name of the Amount field
+            total += opp.amount;
         });
         this.totalAmount = total;
+    }
+
+    handleSort(event) {
+        this.sortBy = event.detail.fieldName;
+        this.sortDirection = event.detail.sortDirection;
+        this.sortData(this.sortBy, this.sortDirection);
+    }
+
+    sortData(fieldname, direction) {
+        const cloneData = [...this.opportunityData];
+        // Return the value stored in the field
+        let keyValue = (a) => {
+            return a[fieldname];
+        };
+        // cheking reverse direction
+        let isReverse = direction === 'asc' ? 1: -1;
+        // sorting data
+        cloneData.sort((x, y) => {
+            x = keyValue(x) ? keyValue(x) : ''; // handling null values
+            y = keyValue(y) ? keyValue(y) : '';
+            // sorting values based on direction
+            return isReverse * ((x > y) - (y > x));
+        });
+        this.opportunityData = cloneData;
     }
     
 }
